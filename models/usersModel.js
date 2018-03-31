77394952561537c442edce4a8887152ae0b2e8c6
subhash viswakarma <putumca@gmail.com>
2018-03-31 09:43:36 +0530

    validation fix

--- models/usersModel.js
+++ models/usersModel.js
@@ -6,37 +6,46 @@ var Schema=mongoose.Schema;
 // ────────────────────────────────────────────────────────────────────────
 //
 var randomPassword=new Date().getTime()
+var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 var UserSchema= new Schema({
-    firstname:{type:String,required:true,min:[3,'too short']},
-    lastname:{type:String,required:true},
-    location:{type:String,required:true},
+    firstname:{type:String,required:[true,'first name is required'],minlength:3},
+    lastname:{type:String,required:[true,'first name is required'],minlength:3},
+    location:{type:String,default:null},
     address:{type:String,default:null},
     category:{type:String,default:null},
     phone: {
         type: String,
         validate: {
-          validator: function(v) {
-            return /\d{3}-\d{3}-\d{4}/.test(v);
-          },
-          message: '{VALUE} is not a valid phone number!'
+            validator: function(v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: '{VALUE} is not a valid phone number!'
         },
         required: [true, 'User phone number required']
-      },
-    email:{type:String,default:null,unique:true},
-    password:{type:String,default:randomPassword,select:false},
-    state:{type:String,default:null},
-    city:{type:String,default:null},
-    zip:{type:String,default:null},
-    country:{type:String,default:null,required:true},
-    rating:{type:Number,default:0},
-    shopname:{type:String,default:null},
-    online:{type:String,default:null},
-    profile_image:{type:String,default:null,required:true},
-    type:{type:String,default:null,required:true},
-    facebook_id:{type:String,default:null},
-    about:{type:String,default:null},
-    birthday:{type:String,default:null},
-    category:{type:Schema.Types.ObjectId,ref:'Category',default:null}
+    },
+    email:{type:String,  validate: {
+        validator: function(v) {
+            return emailRegex.test(v);
+        },
+        message: '{VALUE} is not a valid email!'
+    },
+    required: [true, 'Email required']
+    
+},
+password:{type:String,default:randomPassword,select:false},
+state:{type:String,default:null},
+city:{type:String,default:null},
+zip:{type:String,default:null},
+country:{type:String,default:null,required:true},
+rating:{type:Number,default:0},
+shopname:{type:String,default:null},
+online:{type:String,default:null},
+profile_image:{type:String,default:null,required:true},
+type:{type:String,default:null,required:true},
+facebook_id:{type:String,default:null},
+about:{type:String,default:null},
+birthday:{type:String,default:null},
+category:{type:Schema.Types.ObjectId,ref:'Category',default:null}
 },{
     timestamps:true,
     typecast:true
